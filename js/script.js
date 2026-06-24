edb5dfe8de08669b5878e326935a59c68419601e
unknown <abdullahmalik6960063@gmail.com>
2026-06-24 11:10:55 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    document.getElementById("navLinks")
+    .classList.toggle("active");
+}
+
+function validateForm() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+
+    if(name === "" || email === ""){
+        alert("Please fill all fields");
+        return false;
+    }
+
+    alert("Form Submitted Successfully");
+    return true;
+}
